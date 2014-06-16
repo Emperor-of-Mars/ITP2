f4baa3c424c12f7990bcfb5e758a692ad31fa99b
Erik Skalla <erik.skalla@gmail.com>
2014-06-16 16:59:59 +0200

    backbuttons in credits und highscores

--- credits.cpp
+++ credits.cpp
@@ -8,13 +8,17 @@ Credits::Credits(){
 }
 
 void Credits::credits_view(SDL_Event *event){
+    bool quit = false;
     Timer frametimer;
-    Texture t_background, t_credits_betreuer, t_credits_teamleader, t_credits_programmer, t_credits_programmer_name;
+    Texture t_background, t_credits_betreuer, t_credits_teamleader, t_credits_programmer, t_credits_programmer_name, buttonTexture;
+    Button backToMenu;
     SDL_Color textColor = {255, 64, 64, 255};
     string credits_betreuer = "Betreuer:        Rebin Jaber";
     string credits_teamleader = "Teamleader:   Erik Skalla";
     string credits_programmer = "Executive Programmer:";
-    if(!t_background.loadFromFile("res/Stars_Background.png")){
+    if(!t_background.loadFromFile("res/Stars_Background.png") ||
+       !buttonTexture.loadFromFile("res/button.png") ||
+       !backToMenu.init(&buttonTexture, 225, "Back", textColor)){
 		cout << "Failed to load resources!" << endl;
 		close_SDL();
 		return;
@@ -32,8 +36,25 @@ void Credits::credits_view(SDL_Event *event){
 	t_credits_programmer.setPosition((float)SCREEN_WIDTH * 0.1, (float)SCREEN_HEIGHT * 0.4);
 	t_credits_programmer_name.setPosition((float)SCREEN_WIDTH * 0.3, (float)SCREEN_HEIGHT * 0.5);
 
+    backToMenu.setScale((float)SCREEN_WIDTH / BASE_SCREEN_WIDTH, (float)SCREEN_HEIGHT / BASE_SCREEN_HEIGHT);
+    backToMenu.setPosition(SCREEN_WIDTH *0.1 - backToMenu.getWidth() * 0.5, SCREEN_HEIGHT * 0.8 - backToMenu.getHeight() / 2);
     SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0xFF, 0xFF );
     SDL_RenderClear(gRenderer);
+
+    while(!quit){
+        frametimer.start();
+		while(SDL_PollEvent(event) != 0){
+			if(event->type == SDL_QUIT)quit = true;
+			else if(event->type == SDL_KEYDOWN){
+				switch(event->key.keysym.sym){
+				case SDLK_q:
+					quit = true;
+					break;
+				}
+			}
+		}
+
+    if(backToMenu.handleEvent(event) == 1)quit = true;
 
     t_background.render(2);
     t_credits_betreuer.render();
@@ -53,10 +74,8 @@ void Credits::credits_view(SDL_Event *event){
     t_credits_programmer_name.setScale((float)SCREEN_WIDTH / BASE_SCREEN_WIDTH * 2.5, (float)SCREEN_HEIGHT / BASE_SCREEN_HEIGHT * 3);
     t_credits_programmer_name.render();
 
+    backToMenu.render();
     SDL_RenderPresent(gRenderer);
 
-    while(1){
-        SDL_PollEvent(event);
-        if(event->type == SDL_KEYDOWN) return;
     }
 }
--- highscores.cpp
+++ highscores.cpp
@@ -12,12 +12,16 @@ Highscores::~Highscores() {
 }
 
 void Highscores::highscores_view(SDL_Event *event, XmlHighscore *highsxml){
+    bool quit = false;
     Timer frametimer;
-    Texture t_background, t_Beschreibung, t_Scores;
+    Texture t_background, t_Beschreibung, t_Scores, buttonTexture;
+    Button backToMenu;
     SDL_Color textColor = {255, 64, 64, 255};
     string Beschreibung = "Highscores: ";
     string Scores = to_string(highsxml->showScore(0));
-    if(!t_background.loadFromFile("res/Stars_Background.png")){
+    if(!t_background.loadFromFile("res/Stars_Background.png") ||
+       !buttonTexture.loadFromFile("res/button.png") ||
+       !backToMenu.init(&buttonTexture, 225, "Back", textColor)){
 		cout << "Failed to load resources!" << endl;
 		close_SDL();
 		return;
@@ -26,22 +30,39 @@ void Highscores::highscores_view(SDL_Event *event, XmlHighscore *highsxml){
 	t_Beschreibung.setScale((float)SCREEN_WIDTH / BASE_SCREEN_WIDTH * 2.5, (float)SCREEN_HEIGHT / BASE_SCREEN_HEIGHT * 3);
 	t_Beschreibung.setPosition((float)SCREEN_WIDTH * 0.1, (float)SCREEN_HEIGHT * 0.45);
 
+    backToMenu.setScale((float)SCREEN_WIDTH / BASE_SCREEN_WIDTH, (float)SCREEN_HEIGHT / BASE_SCREEN_HEIGHT);
+    backToMenu.setPosition(SCREEN_WIDTH *0.1 - backToMenu.getWidth() * 0.5, SCREEN_HEIGHT * 0.8 - backToMenu.getHeight() / 2);
+
+    while(!quit){
+        frametimer.start();
+		while(SDL_PollEvent(event) != 0){
+			if(event->type == SDL_QUIT)quit = true;
+			else if(event->type == SDL_KEYDOWN){
+				switch(event->key.keysym.sym){
+				case SDLK_q:
+					quit = true;
+					break;
+				}
+			}
+		}
+
+    if(backToMenu.handleEvent(event) == 1)quit = true;
+
     SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0xFF, 0xFF );
     SDL_RenderClear(gRenderer);
     t_background.render(2);
     t_Beschreibung.render();
 
     for (int i = 0; i<10; i++) {
-    Scores = to_string(highsxml->showScore(i));
-    t_Scores.loadFromRenderedText(Scores.c_str(), textColor);
-    t_Scores.setPosition((float)SCREEN_WIDTH * 0.6, (float)SCREEN_HEIGHT * i/10);
-    t_Scores.setScale((float)SCREEN_WIDTH / BASE_SCREEN_WIDTH * 2.5, (float)SCREEN_HEIGHT / BASE_SCREEN_HEIGHT * 3);
-    t_Scores.render();
-    }
+        Scores = to_string(highsxml->showScore(i));
+        t_Scores.loadFromRenderedText(Scores.c_str(), textColor);
+        t_Scores.setPosition((float)SCREEN_WIDTH * 0.6, (float)SCREEN_HEIGHT * i/10);
+        t_Scores.setScale((float)SCREEN_WIDTH / BASE_SCREEN_WIDTH * 2.5, (float)SCREEN_HEIGHT / BASE_SCREEN_HEIGHT * 3);
+        t_Scores.render();
+        }
+    backToMenu.render();
+
     SDL_RenderPresent(gRenderer);
 
-    while(1){
-        SDL_PollEvent(event);
-        if(event->type == SDL_KEYDOWN) return;
     }
 }
